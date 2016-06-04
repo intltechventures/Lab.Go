a8380753bf8547593cd3464b1ad236478fdff30f
Kelvin D. Meeks <kmeeks@intltechventures.com>
2016-06-04 14:15:23 -0700

    formatted src

--- Examples/TypeConversion/src/TypeConversion/implicit_type_conversion.go
+++ Examples/TypeConversion/src/TypeConversion/implicit_type_conversion.go
@@ -1,19 +1,19 @@
-// An example of Implicit Type Conversion 
+// An example of Implicit Type Conversion
 // From a Twitter post by Dave DHeny
 // https://twitter.com/davecheney/status/734646224696016901
 // See:
 // https://play.golang.org/p/0HfZFFnB7M
 package main
+
 import "fmt"
 
-type stack []uintptr 
-
+type stack []uintptr
 
 // an anon type can be converted to a named type implicitly
-func callers() stack { 
-  return make([]uintptr, 20) 
+func callers() stack {
+	return make([]uintptr, 20)
 }
 
 func main() {
-    fmt.Println(callers() [0])
+	fmt.Println(callers()[0])
 }
